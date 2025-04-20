b0f3b55a0c2c5c8fd96bec671d16e7d1bf132643
Chi-kwan Chan <chanc@arizona.edu>
2025-04-19 23:00:12 -0700

    Add empty files

---
+++ src/phys305_hw6/__init__.py
@@ -0,0 +1,5 @@
+from .a1 import *
+from .a2 import *
+from .a3 import *
+from .a4 import *
+from .a5 import *
--- tests/test_a5.py
+++ tests/test_a5.py
@@ -4,7 +4,7 @@ import os
 
 def test_main():
 
-    main(Nt=10)
+    main(Nt=10, skip=1)
 
     for n in range(10+1):
-        assert exists(f'{n:04d}.png'))
+        assert exists(f'{n:04d}.png')
